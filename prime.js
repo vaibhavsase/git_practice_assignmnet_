d8564ea9f50b411c026225914964d1c5ab3b9c08
unknown <sasevaibhav00@gmail.com>
2022-11-02 17:46:03 +0530

    code uploaded

---
+++ prime.js
@@ -0,0 +1,12 @@
+let N=13
+let fact=0
+for(let i=1;i<=N;i++){
+    if(N%i===0){
+        fact++
+    }
+}
+if(fact==3){
+    console.log(N+" is prime number")
+}else{
+    console.log(N+" is not prime number")
+}
